b5f77273f88d7b80189dc910bb389fa98393c228
Amanna <amannawongbu@gmail.com>
2023-04-15 23:22:42 -0700

    Start of Loading Screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Button } from "react-native";
+import { StyleSheet, View, Button, Image } from "react-native";
 import { Text } from "@rneui/themed";
 
 export default function WelcomeScreen({ navigation }) {
@@ -6,49 +6,7 @@ export default function WelcomeScreen({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <Text h1>
-                This is the Welcome screen.
-            </Text>
-            <Button
-                title="Home"
-                raised={true}
-                onPress={() => navigation.navigate('TabNavigator')}
-            />
-            <Button
-                title="SignUp"
-                raised={true}
-                onPress={() => navigation.navigate('SignUp')}
-            />
-            <Button
-                title="LogIn"
-                raised={true}
-                onPress={() => navigation.navigate('LogIn')}
-            />
-            <Button
-                title="Onboarding1"
-                raised={true}
-                onPress={() => navigation.navigate('Onboarding1')}
-            />
-            <Button
-                title="Onboarding2"
-                raised={true}
-                onPress={() => navigation.navigate('Onboarding2')}
-            />
-            <Button
-                title="Onboarding3"
-                raised={true}
-                onPress={() => navigation.navigate('Onboarding3')}
-            />
-            <Button
-                title="Onboarding4"
-                raised={true}
-                onPress={() => navigation.navigate('Onboarding4')}
-            />
-            <Button
-                title="Onboarding5"
-                raised={true}
-                onPress={() => navigation.navigate('Onboarding5')}
-            />
+            <Image source={require('../assets/images/24Frames_Logo.png')} />
         </View>
     );
 }
@@ -58,6 +16,4 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#F5EFDF",
     },
-    MovieList: {},
-    GenreList: {},
 });
