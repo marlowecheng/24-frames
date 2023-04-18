5be02700c90d0a4e384ca2660100f106cf3abd1b
Wood3001 <wood3001@gmail.com>
2023-04-18 14:57:09 -0700

    firebase login/signup implemented

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlir1E6AhctKnI3OGam2YpINlWD_2PRkQ",
+  authDomain: "frames-6ff4a.firebaseapp.com",
+  projectId: "frames-6ff4a",
+  storageBucket: "frames-6ff4a.appspot.com",
+  messagingSenderId: "510399821555",
+  appId: "1:510399821555:web:4a744d31d64ec79436a986"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+const auth = firebase.auth();
+
+export { auth };
