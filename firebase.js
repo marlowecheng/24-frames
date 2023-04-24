f3a5581f9bb0b1cab5b5ad672e07bf98ed42eb96
Wood3001 <wood3001@gmail.com>
2023-04-23 22:24:57 -0700

    firebase init updates

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth';
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,9 +18,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// if (!firebase.apps.length) {
-//     firebase.initializeApp(firebaseConfig)
-// }
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
 
 const app = initializeApp(firebaseConfig);
 
